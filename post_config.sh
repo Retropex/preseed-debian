8aaf71660ee51759247996417be91bd351f54159
Léo Haf <leo@haf.ovh>
2026-05-26 15:04:22 +0200

    Better script handling

--- post_config.sh
+++ post_config.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
 
+set -e
+
 if ! command -v whiptail &> /dev/null; then
 	echo "Whiptail is not installed, impossible to show post installation steps."
 	echo "Please run \"sudo apt install whiptail -y\" then logout and login again"
 	exit 1
 fi
 
-whiptail --msgbox "Thanks for mining with us!\nLet's do the last steps to configure your datumbox." 0 0
+whiptail --msgbox "Thanks for mining with us!\nLet's do the last steps to configure your datum box." 0 0
 
 PASSWORD="todo"
 
@@ -18,15 +20,29 @@ do
 	if [ "$PASSWORD" != "$PASSWORD2" ]; then
 		NEWT_COLORS="root=white,red" whiptail --msgbox "Both password didn't match, please try again." 0 0
 	fi
+	
+	if [ "$PASSWORD" = "" ]; then
+		NEWT_COLORS="root=white,red" whiptail --msgbox "Password must not be empty" 0 0
+		PASSWORD="todo"
+	fi
 done
 
-if echo 'leo:$PASSWORD' | chpasswd; then
+if echo "leo:$PASSWORD" | chpasswd; then
 	NEWT_COLORS="root=white,green" whiptail --msgbox "Password sucessfully changed!" 0 0
 else
 	NEWT_COLORS="root=white,red" whiptail --msgbox "Failed to change password." 0 0
 fi
 
-whiptail --msgbox "We will now proceed to the Bitcoin node and DATUM Gateway configuration." 0 0
-dpkg-reconfigure -pmedium bitcoin-knots datum-gateway
+unset PASSWORD PASSWORD2
 
-whiptail --msgbox "You datum box is now configured!" 0 0
+whiptail --msgbox "We will now proceed to the Bitcoin node and DATUM Gateway configuration." 0 0
+
+if ! dpkg-reconfigure -plow bitcoin-knots datum-gateway; then
+	NEWT_COLORS="root=white,red" whiptail --msgbox "Failed to reconfigure Bitcoin Knots and/or DATUM Gateway" 0 0
+fi
+
+whiptail --msgbox "Your datum box is now configured!" 0 0
+
+if [ -f /home/box/need_config ]; then
+	rm /home/box/need_config
+fi
